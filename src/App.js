e1cedfd7197998a2255121228b145b31866b516b
Marwa Elali <marwaelali@gmail.com>
2023-02-17 10:55:50 -0500

    edit main app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import MovieQuiz from "./components/MovieQuiz";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MOVIE POSTER QUIZ
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-body">
+        <MovieQuiz />
+      </div>
     </div>
   );
 }
